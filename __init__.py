facfa56cfc2322fd58dea0827568562fbc0cbdf9
Razorflame3912 <melsharawy@stuy.edu>
2018-05-21 21:50:05 -0400

    started work on a demo

---
+++ __init__.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, session, redirect, url_for, flash
+import sys
+import os
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+app = Flask(__name__)
+
+app.secret_key = 'keysmithsmakekeys'
+
+
+@app.route('/')                                                                 
+def root():
+    return render_template('root.html')
+
+@app.route('/chat' , methods=['POST','GET'])
+def about():
+    name = request.form['chatname']
+    room = request.form['room']
+    #session['name'] = name
+    #session['room'] = room
+    return render_template('chat.html', room=room)
+    
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
---
+++ static/js/chat.js
@@ -0,0 +1,4 @@
+console.log("Hello World!");
+var header = $.("h1")[0];
+var room = header.id;
+console.log(room);
